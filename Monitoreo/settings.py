54c48c552b586104801e6ab7b871616da897cf8b
ZERO20 <edgar-dcv@hotmail.com>
2017-09-11 12:51:53 -0500

    change db backend project

--- Monitoreo/settings.py
+++ Monitoreo/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'Monitoreo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'monitor',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'monitor.sqlite3'),
     }
 }
 
